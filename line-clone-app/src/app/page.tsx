39660c6d4b83cb5e23aec82ecbde657eddafb546
snow-saboten <snow.saboten74@gmail.com>
2024-02-28 08:31:45 +0900

    shadcnuiの動作確認

--- line-clone-app/src/app/page.tsx
+++ line-clone-app/src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="bg-zinc-600 text-white">Hello world!</div>
+    <div className="bg-zinc-600 text-white">
+      Hello world!
+      <Button>ボタン</Button>
+    </div>
   );
 }
